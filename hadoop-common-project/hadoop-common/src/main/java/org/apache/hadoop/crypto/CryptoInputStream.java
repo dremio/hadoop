b811c258973c00e527084f39dc2d9682177cdfd5
Sahil Takiar <stakiar@apache.org>
2019-02-20 11:36:37 -0800

    DX-18109: Add pread with ByteBuffer API in hadoop client
    Cherry-pick and fix conflicts for the following changes from trunk
    
    HDFS-14267. Add test_libhdfs_ops to libhdfs tests, mark libhdfs_read/write.c as examples.
    HDFS-14111. hdfsOpenFile on HDFS causes unnecessary IO from file offset 0.
    HDFS-3246: pRead equivalent for direct read path (#597)
    
    Change-Id: I883812e8bb5f0e951a2523336d63022ab61cf737
    (cherry picked from commit 01c0d7df1add14c761561c7c640a33d0627165e9)

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/crypto/CryptoInputStream.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/crypto/CryptoInputStream.java
@@ -386,6 +386,14 @@ public class CryptoInputStream extends FilterInputStream implements
       // This operation does not change the current offset of the file
       decrypt(position, buf, n, bufPos);
     }
+    int bufPos = buf.position();
+    final int n = ((ByteBufferPositionedReadable) in).read(position, buf);
+    if (n > 0) {
+      // This operation does not change the current offset of the file
+      decrypt(position, buf, n, bufPos);
+    }
+
+    return n;
   }
 
   /**
@@ -538,7 +546,8 @@ public class CryptoInputStream extends FilterInputStream implements
     } else {
       if (!(in instanceof Seekable)) {
         throw new UnsupportedOperationException(in.getClass().getCanonicalName()
-            + " does not support seek.");
+            + " does not " +
+                "support seek.");
       }
       ((Seekable) in).seek(pos);
       resetStreamOffset(pos);
@@ -714,13 +723,19 @@ public class CryptoInputStream extends FilterInputStream implements
         throw new UnsupportedOperationException(in.getClass().getCanonicalName()
             + " does not support seek.");
       }
-      // Have some decrypted data unread, need to reset.
+      if (!(in instanceof Seekable)) {
+        throw new UnsupportedOperationException("This stream does not " +
+                "support seek.");
+      }// Have some decrypted data unread, need to reset.
       ((Seekable) in).seek(getPos());
       resetStreamOffset(getPos());
     }
     if (!(in instanceof HasEnhancedByteBufferAccess)) {
       throw new UnsupportedOperationException(in.getClass().getCanonicalName()
           + " does not support enhanced byte buffer access.");
+    }if (!(in instanceof HasEnhancedByteBufferAccess)) {
+      throw new UnsupportedOperationException("This stream does not support " +
+          "enhanced byte buffer access.");
     }
     final ByteBuffer buffer = ((HasEnhancedByteBufferAccess) in).
         read(bufferPool, maxLength, opts);
