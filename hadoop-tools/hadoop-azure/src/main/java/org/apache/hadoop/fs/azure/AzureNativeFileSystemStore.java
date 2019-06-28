9fe57bb9a4a4d44204362b72623be974ef8f7d57
TiffanyLam <tiffany@dremio.com>
2019-06-28 11:25:52 -0700

    DX-17276 Add Azure AD support in Dremio's hadoop-azure library for Storage V1 support
    - hadoop-azure WASB(S) file system for Storage V1 connections does not support OAuth 2.0 as an authentication method, add support in the hadoop-azure library such that hadoop FileSystem implementation for Azure Blobs can connect with OAuth
    
    Change-Id: I0434d47de2c8f5bc9d865a4d63b276593930a6fb
    (cherry picked from commit 6ae4080433a9da05fb986f6ae02c988482a3f83b)

--- hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azure/AzureNativeFileSystemStore.java
+++ hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azure/AzureNativeFileSystemStore.java
@@ -56,6 +56,8 @@ import org.apache.hadoop.fs.azure.metrics.AzureFileSystemInstrumentation;
 import org.apache.hadoop.fs.azure.metrics.BandwidthGaugeUpdater;
 import org.apache.hadoop.fs.azure.metrics.ErrorMetricUpdater;
 import org.apache.hadoop.fs.azure.metrics.ResponseReceivedMetricUpdater;
+import org.apache.hadoop.fs.azurebfs.oauth2.AzureADAuthenticator;
+import org.apache.hadoop.fs.azurebfs.oauth2.AzureADToken;
 import org.apache.hadoop.fs.permission.FsPermission;
 import org.apache.hadoop.fs.permission.PermissionStatus;
 import org.apache.hadoop.io.IOUtils;
@@ -72,6 +74,7 @@ import com.microsoft.azure.storage.RetryNoRetry;
 import com.microsoft.azure.storage.StorageCredentials;
 import com.microsoft.azure.storage.StorageCredentialsAccountAndKey;
 import com.microsoft.azure.storage.StorageCredentialsSharedAccessSignature;
+import com.microsoft.azure.storage.StorageCredentialsToken;
 import com.microsoft.azure.storage.StorageErrorCodeStrings;
 import com.microsoft.azure.storage.StorageException;
 import com.microsoft.azure.storage.Constants;
@@ -164,6 +167,15 @@ public class AzureNativeFileSystemStore implements NativeFileSystemStore {
   private static final String KEY_AUTO_THROTTLE_ENABLE = "fs.azure.autothrottling.enable";
 
   private static final String KEY_ENABLE_STORAGE_CLIENT_LOGGING = "fs.azure.storage.client.logging";
+
+  //Configuration properties values for Azure Active Directory
+  private static final String AUTH_TYPE_OAUTH = "OAUTH";
+  private static final String KEY_AUTH_DEFAULT = "NOT_OAUTH";
+  //Configuration properties for Azure Active Directory
+  private static final String KEY_AUTH_TYPE = "fs.azure.account.auth.type";
+  private static final String KEY_AD_CLIENT_ID = "fs.azure.account.oauth2.client.id.";
+  private static final String KEY_AD_TOKEN_ENDPOINT = "fs.azure.account.oauth2.client.endpoint.";
+  private static final String KEY_AD_CLIENT_SECRET = "fs.azure.account.oauth2.client.secret.";
 
   /**
    * Configuration keys to identify if WASB needs to run in Secure mode. In Secure mode
@@ -348,6 +360,9 @@ public class AzureNativeFileSystemStore implements NativeFileSystemStore {
   private boolean useSecureMode = false;
   private boolean useLocalSasKeyMode = false;
 
+  // OAuth
+  private boolean useOAuth = false;
+
   // User-Agent
   private String userAgentId;
 
@@ -530,8 +545,13 @@ public class AzureNativeFileSystemStore implements NativeFileSystemStore {
     useLocalSasKeyMode = conf.getBoolean(KEY_USE_LOCAL_SAS_KEY_MODE,
         DEFAULT_USE_LOCAL_SAS_KEY_MODE);
 
+    conf.setIfUnset(KEY_AUTH_TYPE, KEY_AUTH_DEFAULT);
+    useOAuth = AUTH_TYPE_OAUTH.equals(conf.get(KEY_AUTH_TYPE));
+
     if (null == this.storageInteractionLayer) {
-      if (!useSecureMode) {
+      if (useOAuth) {
+        this.storageInteractionLayer = new OAuthStorageInterfaceImpl();
+      } else if (!useSecureMode) {
         this.storageInteractionLayer = new StorageInterfaceImpl();
       } else {
         this.storageInteractionLayer = new SecureStorageInterfaceImpl(
@@ -963,6 +983,42 @@ public class AzureNativeFileSystemStore implements NativeFileSystemStore {
         DEFAULT_STORAGE_EMULATOR_ACCOUNT_NAME));
   }
 
+  private void connectUsingAzureAD(String accountName, String containerName, AzureADCredentials adCredentials)
+          throws IOException, StorageException, URISyntaxException {
+
+    final AzureADToken token = AzureADAuthenticator.getTokenUsingClientCreds(adCredentials.getTokenEndpoint(),
+            adCredentials.getClientId(), adCredentials.getClientSecret());
+    OAuthStorageInterfaceImpl oAuthStorageInterface = (OAuthStorageInterfaceImpl) this.storageInteractionLayer;
+    oAuthStorageInterface.setToken(token);
+    StorageCredentials tokenCredentials = new StorageCredentialsToken(accountName, token.getAccessToken());
+
+    URI blobEndPoint;
+    if (isStorageEmulatorAccount(accountName)) {
+      isStorageEmulator = true;
+      CloudStorageAccount account =
+              CloudStorageAccount.getDevelopmentStorageAccount();
+      storageInteractionLayer.createBlobClient(account);
+    } else {
+      blobEndPoint = new URI(getHTTPScheme() + "://" + accountName);
+      storageInteractionLayer.createBlobClient(blobEndPoint, tokenCredentials);
+    }
+    suppressRetryPolicyInClientIfNeeded();
+
+    container = storageInteractionLayer.getContainerReference(containerName);
+    rootDirectory = container.getDirectoryReference("");
+  }
+
+  @VisibleForTesting
+  public static AzureADCredentials getAzureADCredentialsFromConfiguration(String accountName, Configuration conf) {
+    return new AzureADCredentials(conf.get(getFullyQualifiedKey(accountName, KEY_AD_CLIENT_ID)),
+            conf.get(getFullyQualifiedKey(accountName, KEY_AD_TOKEN_ENDPOINT)),
+            conf.get(getFullyQualifiedKey(accountName, KEY_AD_CLIENT_SECRET)));
+  }
+
+  private static String getFullyQualifiedKey(String accountName, String key) {
+    return key + accountName;
+  }
+
   @VisibleForTesting
   public static String getAccountKeyFromConfiguration(String accountName,
       Configuration conf) throws KeyProviderException {
@@ -1087,6 +1143,14 @@ public class AzureNativeFileSystemStore implements NativeFileSystemStore {
         connectUsingConnectionStringCredentials(
             getAccountFromAuthority(sessionUri),
             getContainerFromAuthority(sessionUri), propertyValue);
+        return;
+      }
+      if (useOAuth) {
+        AzureADCredentials adCredentials = getAzureADCredentialsFromConfiguration(accountName, sessionConfiguration);
+        OAuthStorageInterfaceImpl oAuthStorageInterface = (OAuthStorageInterfaceImpl) this.storageInteractionLayer;
+        oAuthStorageInterface.setAdCredentials(adCredentials);
+        connectUsingAzureAD(accountName, containerName, adCredentials);
+        return;
       } else {
         LOG.debug("The account access key is not configured for {}. "
             + "Now try anonymous access.", sessionUri);
@@ -1886,6 +1950,7 @@ public class AzureNativeFileSystemStore implements NativeFileSystemStore {
    */
   private Iterable<ListBlobItem> listRootBlobs(boolean includeMetadata,
       boolean useFlatBlobListing) throws StorageException, URISyntaxException {
+    checkAndUpdateClient();
     return rootDirectory.listBlobs(
         null,
         useFlatBlobListing,
@@ -1918,6 +1983,7 @@ public class AzureNativeFileSystemStore implements NativeFileSystemStore {
   private Iterable<ListBlobItem> listRootBlobs(String aPrefix, boolean includeMetadata,
       boolean useFlatBlobListing) throws StorageException, URISyntaxException {
 
+    checkAndUpdateClient();
     Iterable<ListBlobItem> list = rootDirectory.listBlobs(aPrefix,
         useFlatBlobListing,
         includeMetadata
@@ -1956,6 +2022,7 @@ public class AzureNativeFileSystemStore implements NativeFileSystemStore {
       EnumSet<BlobListingDetails> listingDetails, BlobRequestOptions options,
       OperationContext opContext) throws StorageException, URISyntaxException {
 
+    checkAndUpdateClient();
     CloudBlobDirectoryWrapper directory =  this.container.getDirectoryReference(aPrefix);
     return directory.listBlobs(
         null,
@@ -1963,6 +2030,19 @@ public class AzureNativeFileSystemStore implements NativeFileSystemStore {
         listingDetails,
         options,
         opContext);
+  }
+
+  private synchronized void checkAndUpdateClient() throws StorageException {
+    if(useOAuth) {
+      OAuthStorageInterfaceImpl oAuthStorage = (OAuthStorageInterfaceImpl) storageInteractionLayer;
+      if(oAuthStorage.isTokenAboutToExpire()) {
+        try {
+          oAuthStorage.updateTokenAndClient();
+        } catch(IOException ex) {
+          throw StorageException.translateClientException(ex);
+        }
+      }
+    }
   }
 
   /**
@@ -1983,8 +2063,10 @@ public class AzureNativeFileSystemStore implements NativeFileSystemStore {
 
     CloudBlobWrapper blob = null;
     if (isPageBlobKey(aKey)) {
+      checkAndUpdateClient();
       blob = this.container.getPageBlobReference(aKey);
     } else {
+      checkAndUpdateClient();
       blob = this.container.getBlockBlobReference(aKey);
     blob.setStreamMinimumReadSizeInBytes(downloadBlockSizeBytes);
     blob.setWriteBlockSizeInBytes(uploadBlockSizeBytes);
@@ -2315,8 +2397,10 @@ public class AzureNativeFileSystemStore implements NativeFileSystemStore {
 
       Iterable<ListBlobItem> objects;
       if (prefix.equals("/")) {
+        checkAndUpdateClient();
         objects = listRootBlobs(true, enableFlatListing);
       } else {
+        checkAndUpdateClient();
         objects = listRootBlobs(prefix, true, enableFlatListing);
       }
 
@@ -3032,6 +3116,7 @@ public class AzureNativeFileSystemStore implements NativeFileSystemStore {
         throw new UnsupportedOperationException("Append not supported for Page Blobs");
       }
 
+      checkAndUpdateClient();
       CloudBlobWrapper blob =  this.container.getBlockBlobReference(key);
 
       OutputStream outputStream;
