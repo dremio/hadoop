a5ad1ba602cac8df13f12f182296c43166ea6be7
Kyle Porter <kporter@dremio.com>
2019-11-05 22:27:38 -0800

    DX-19314: make new kms format configurable
    Fix some bad typos on the prior patch.
    
    Older versions of Hadoop cannot accept the new kms://
    prefix and fail, so make it configurable.
    
    Change-Id: Ibd6deb44c5724c1f5fcc165f5d30bc0310793d54

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/CommonConfigurationKeysPublic.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/CommonConfigurationKeysPublic.java
@@ -885,6 +885,16 @@ public class CommonConfigurationKeysPublic {
    * <a href="{@docRoot}/../hadoop-project-dist/hadoop-common/core-default.xml">
    * core-default.xml</a>
    */
+  public static final String KMS_CLIENT_USE_LEGACY_TOKEN_KEY =
+          "hadoop.security.kms.client.use-legacy-token";
+  /** Default value is enabled. */
+  public static final boolean KMS_CLIENT_USE_LEGACY_TOKEN_DEFAULT = false;
+
+  /**
+   * @see
+   * <a href="{@docRoot}/../hadoop-project-dist/hadoop-common/core-default.xml">
+   * core-default.xml</a>
+   */
   public static final String HADOOP_SECURITY_JAVA_SECURE_RANDOM_ALGORITHM_KEY = 
     "hadoop.security.java.secure.random.algorithm";
   /** Defalt value for HADOOP_SECURITY_JAVA_SECURE_RANDOM_ALGORITHM_KEY */
