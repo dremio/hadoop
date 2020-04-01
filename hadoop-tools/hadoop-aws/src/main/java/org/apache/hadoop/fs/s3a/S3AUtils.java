5827e24d4d3113ba7580683df5f24c00678d9b8a
Brandon Huang <brandon@dremio.com>
2020-04-01 15:42:57 -0700

    DX-21904: Support S3 requester-pays headers
    Change-Id: I4b3bfcb17a6a409f04662f3d562ce4d6c4e3623a

--- hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3AUtils.java
+++ hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3AUtils.java
@@ -1299,6 +1299,11 @@ public final class S3AUtils {
       ClientConfiguration awsConf) throws IOException {
     awsConf.setMaxConnections(intOption(conf, MAXIMUM_CONNECTIONS,
         DEFAULT_MAXIMUM_CONNECTIONS, 1));
+    boolean secureConnections = conf.getBoolean(SECURE_CONNECTIONS,
+        DEFAULT_SECURE_CONNECTIONS);
+    boolean requesterPays = conf.getBoolean(ALLOW_REQUESTER_PAYS,
+            DEFAULT_ALLOW_REQUESTER_PAYS);
+    awsConf.setProtocol(secureConnections ?  Protocol.HTTPS : Protocol.HTTP);
     initProtocolSettings(conf, awsConf);
     awsConf.setMaxErrorRetry(intOption(conf, MAX_ERROR_RETRIES,
         DEFAULT_MAX_ERROR_RETRIES, 0));
@@ -1324,6 +1329,9 @@ public final class S3AUtils {
     if (!signerOverride.isEmpty()) {
      LOG.debug("Signer override = {}", signerOverride);
       awsConf.setSignerOverride(signerOverride);
+    }
+    if (requesterPays) {
+      awsConf.addHeader("x-amz-request-payer", "requester");
     }
   }
 
