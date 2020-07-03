c78e6191e7b6e8255f08e282352896bf6fe43a39
subba <subba@dremio.com>
2020-07-03 20:32:00 +0530

    DX-18552: Make file status check optional in S3AFileSystem create()
    Change-Id: Ide0a99fa198447158477f5d9564a4ecd0f9c5c55

--- hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3AFileSystem.java
+++ hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3AFileSystem.java
@@ -265,6 +265,18 @@ public class S3AFileSystem extends FileSystem implements StreamCapabilities,
    */
   public static final int DEFAULT_BLOCKSIZE = 32 * 1024 * 1024;
 
+  /**
+   * This declared delete as idempotent.
+   * This is an "interesting" topic in past Hadoop FS work.
+   * Essentially: with a single caller, DELETE is idempotent
+   * but in a shared filesystem, it is is very much not so.
+   * Here, on the basis that isn't a filesystem with consistency guarantees,
+   * retryable results in files being deleted.
+  */
+  public static final boolean DELETE_CONSIDERED_IDEMPOTENT = true;
+
+  private boolean doCreateFileStatusCheck = CREATE_FILE_STATUS_CHECK_DEFAULT;
+
   private URI uri;
   private Path workingDir;
   private String username;
@@ -417,6 +429,7 @@ public class S3AFileSystem extends FileSystem implements StreamCapabilities,
    */
   public void initialize(URI name, Configuration originalConf)
       throws IOException {
+    doCreateFileStatusCheck = originalConf.getBoolean(CREATE_FILE_STATUS_CHECK, CREATE_FILE_STATUS_CHECK_DEFAULT);
     // get the host; this is guaranteed to be non-null, non-empty
     bucket = name.getHost();
     AuditSpan span = null;
@@ -1673,30 +1686,25 @@ public class S3AFileSystem extends FileSystem implements StreamCapabilities,
       long blockSize,
       Progressable progress) throws IOException {
     String key = pathToKey(path);
-    FileStatus status = null;
-    try {
-      // get the status or throw an FNFE.
-      // when overwriting, there is no need to look for any existing file,
-      // and attempting to do so can poison the load balancers with 404
-      // entries.
-      status = innerGetFileStatus(path, false,
-          overwrite
-              ? StatusProbeEnum.DIRECTORIES
-              : StatusProbeEnum.ALL);
+    if (!overwrite || doCreateFileStatusCheck) {
+      try {
+        // get the status or throw an FNFE
+        final FileStatus status = getFileStatus(path);
 
-      // if the thread reaches here, there is something at the path
-      if (status.isDirectory()) {
-        // path references a directory: automatic error
-        throw new FileAlreadyExistsException(path + " is a directory");
-      }
-      if (!overwrite) {
-        // path references a file and overwrite is disabled
-        throw new FileAlreadyExistsException(path + " already exists");
-      }
-      LOG.debug("Overwriting file {}", path);
-    } catch (FileNotFoundException e) {
-      // this means the file is not found
+        // if the thread reaches here, there is something at the path
+        if (status.isDirectory()) {
+          // path references a directory: automatic error
+          throw new FileAlreadyExistsException(path + " is a directory");
+        }
+        if (!overwrite) {
+          // path references a file and overwrite is disabled
+          throw new FileAlreadyExistsException(path + " already exists");
+        }
+        LOG.debug("Overwriting file {}", path);
+      } catch (FileNotFoundException e) {
+        // this means the file is not found
 
+      }
     }
     instrumentation.fileCreated();
     PutTracker putTracker =
