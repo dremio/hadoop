c1cdbc8f2947a75a15d67f943bdd7ae04333917e
Neeraj Kulkarni <neeraj.kulkarni@dremio.com>
2026-04-28 08:30:23 +0200

    Revert "DX-21471: Fix checking of use of OAuth credentials with AzureNativeFileSystem"
    This reverts commit 0ca26521b1f90d6b56ccf3d0eec793cb3117967d.

--- hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azure/AzureNativeFileSystemStore.java
+++ hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azure/AzureNativeFileSystemStore.java
@@ -1158,14 +1158,6 @@ public class AzureNativeFileSystemStore implements NativeFileSystemStore {
         return;
       }
 
-      if (useOAuth) {
-        AzureADCredentials adCredentials = getAzureADCredentialsFromConfiguration(accountName, sessionConfiguration);
-        OAuthStorageInterfaceImpl oAuthStorageInterface = (OAuthStorageInterfaceImpl) this.storageInteractionLayer;
-        oAuthStorageInterface.setAdCredentials(adCredentials);
-        connectUsingAzureAD(accountName, containerName, adCredentials);
-        return;
-      }
-
       // Check whether the account is configured with an account key.
       propertyValue = getAccountKeyFromConfiguration(accountName,
           sessionConfiguration);
@@ -1175,6 +1167,13 @@ public class AzureNativeFileSystemStore implements NativeFileSystemStore {
         connectUsingConnectionStringCredentials(
             getAccountFromAuthority(sessionUri),
             getContainerFromAuthority(sessionUri), propertyValue);
+        return;
+      }
+      if (useOAuth) {
+        AzureADCredentials adCredentials = getAzureADCredentialsFromConfiguration(accountName, sessionConfiguration);
+        OAuthStorageInterfaceImpl oAuthStorageInterface = (OAuthStorageInterfaceImpl) this.storageInteractionLayer;
+        oAuthStorageInterface.setAdCredentials(adCredentials);
+        connectUsingAzureAD(accountName, containerName, adCredentials);
         return;
       } else {
         LOG.debug("The account access key is not configured for {}. "
